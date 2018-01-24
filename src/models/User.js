0a3c10d3dd3fad52f748854ade002326e44bbeda
VAIIHO NASCIMBENE <vaiihonascimbene@VAIIHOs-iMac.local>
2018-01-24 23:08:00 +0100

    final cicle progress api for reset password

--- src/models/User.js
+++ src/models/User.js
@@ -55,9 +55,9 @@ schema.methods.generateResetPasswordToken = function generateResetPasswordToken(
 			_id: this._id
 		},
 		process.env.JWT_SECRET,
-		{ expiresIn: "1s"}
+		{ expiresIn: "1h" }
 	);
-};
+}; 
 
 schema.methods.toAuthJSON = function toAuthJSON() {
 	return {
